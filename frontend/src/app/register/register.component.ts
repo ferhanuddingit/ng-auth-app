e67ddf57e93f3ea9e3eaa267558b028289b9bc44
ferhanuddin <ferhan_uddin@yahoo.com>
2020-07-24 16:02:18 +0530

    Initial commit

---
+++ frontend/src/app/register/register.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'
+import { AuthService } from '../auth.service';
+import { Form } from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  registerUserData: any = {
+
+  }
+
+  constructor(private _auth: AuthService, private _router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  registerUser() {
+    this._auth.registerUser(this.registerUserData)
+      .subscribe(
+        res => {
+          localStorage.setItem('token', res.token);
+          this._router.navigate(['/special']);
+        },
+        err => console.log(err)
+      )
+  }
+}
